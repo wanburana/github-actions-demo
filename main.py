472e6a155ba9f9845ce48580ec95b86a9a120519
wanburana <wanjirasak@gmail.com>
2024-09-10 00:26:57 +0700

    remove comment

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route('/<random_string>')
 def return_backwards_string(random_string):
-    # comment
     return ''.join(reversed(random_string))
 
 @app.route('/get-mode')
